b885871f29bac2da57083f511350e9f013930c06
unknown <carolayegt13@gmail.com>
2024-10-19 11:14:12 -0500

    modificacion par

---
+++ scrit.js
@@ -0,0 +1,13 @@
+
+console.log("holaaaaa Mundo ");
+
+for(let cots=1 ; cots<=20; cots++){
+    if (cots%2 === 0){
+        console.log ("numero" + cots + "es par");
+    }else{
+        console.log ("numero " + cots + "es impar")
+
+    }
+}
+
+ 
